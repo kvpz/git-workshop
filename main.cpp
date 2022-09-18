d9f211e3b7e5143e4c0847598f2c609c7b1e4c93
kvpz <unknotted26@gmail.com>
2022-09-18 01:19:10 -0400

    first commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
-// IEEE Git Workshop Example Program
+/* 
+	Git Workshop Example Program
+*/
 #include <iostream>
 #include <string>
 #include <stdio.h>
 
+void print_menu()
+{
+    std::cout << "====== MAIN MENU ======" << std::endl;
+    std::cout << "(a) option a\n";
+    std::cout << "(b) option b\n";
+    std::cout << std::endl;
+}
+
 int main()
 {
-  char menu_option;
-  scanf("%c", &menu_option);
-  
-  switch(menu_option) {
-    case 'a':
-        printf("menu option a selected\n");
-        break;
-    case 'b':
-        printf("menu option b selected\n");
-        break;
-    default:
-        printf("invalid menu option\n");
-  }
-  
-  return 0;
+    while(1) 
+    {
+        print_menu();
+        char menu_option;
+        scanf(" %c", &menu_option);
+        switch(menu_option) 
+        {
+            case 'a':
+                printf("menu option a selected\n");
+                break;
+            case 'b':
+                printf("menu option b selected\n");
+                break;
+            default:
+                printf("invalid menu option\n");
+        }
+    }
+    return 0;
   
 }
+
